ea1155af087154a50f70d988f5eceaae1b07a4c5
linleno-jr <iangiwenlin@gmail.com>
2026-03-03 19:02:54 -0800

    updated docs

--- country_io.py
+++ country_io.py
@@ -1,22 +1,41 @@
 import pandas as pd
 from data.info import column_to_name
 
-# outputs info on a given country
 def output_country_info(df, country: str, columns_needed: list):
+    """ Prints out information on a country
+
+    Args:
+        df (pd.DataFrame: Input dataset
+        country (str): Country to find info on
+        columns_needed (list): Info on the country to pull
+    """
+
     print(f"\nHere is the information on the country {country.title()}!")
 
+    # printing out each column
     for column in columns_needed:
         info_category = column_to_name[column]
         info = df.loc[df["Country"] == country, column].iloc[0]
 
         # making sure info isn't empty
-        if pd.isnull(info):
+        if info == "":
             continue
 
         print(f"\n\n{info_category}: {info}")
 
 # This function will handle all user input
 def get_input(df):
+    """ Gets input from the user, specifically their committee, topic, and country, then returns it.
+
+    Args:
+        df (pd.DataFrame): Input dataset
+
+    Returns:
+        committee (str): user inputted committee
+        topic (str): user inputted topic
+        country (str): user inputted country, validated with dataset
+    """
+
     print("Welcome to the automated country profile generator!\n")
 
     # getting the user's committe, topic, and country
@@ -26,7 +45,7 @@ def get_input(df):
     while True:
         country = input("\nWhat is your country? ").lower();
 
-        # making sure user's country is valid
+        # making sure user's country is valid, if it is, return values, else redo process
         if country.lower() in df["Country"].values:
             print(f"\nYour committee: {committee}\nYour topic: {topic}\nYour country: {country.title()}")
             input("\nPress enter to continue, or Ctrl + c to cancel: ")
--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import pandas as pd
 from country_io import output_country_info, get_input
 from data.info import columns_needed
 
-# creating a data frame from csv
+# creating a data frame from csv file that contains data needed
 df = pd.read_csv("data/filtered_countries.csv")
 
-# converting country names in data frame to lowercase
+# converting country names in data frame to lowercase for easier access
 df["Country"] = df["Country"].str.lower()
 
+# main function
 def main():
 
     # getting input from the user
     committee, topic, country = get_input(df)
 
+    # outputting info on the country based on the user's input
     output_country_info(df, country, columns_needed)
 
 if __name__ == "__main__":
-    main()
+    # running main function, handling keyboard interrupt error
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nCancelled by user.")
